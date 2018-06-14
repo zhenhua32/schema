85bc396c58edd7dff928fad5f6c0df96d8d905eb
semantic-release <semantic-release>
2018-06-14 17:38:41 +0300

    0.7.2

--- schema.py
+++ schema.py
@@ -4,7 +4,7 @@ parsing, converted from JSON/YAML (or something else) to Python data-types."""
 
 import re
 
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 __all__ = ['Schema',
            'And', 'Or', 'Regex', 'Optional', 'Use', 'Forbidden', 'Const',
            'SchemaError',
